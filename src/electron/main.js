06cce4467e04a0e630bf2e113017c0ecb80bf46d
Gabriel B <gabriel@Canadas-MacBook-Pro-2.local>
2025-01-06 11:52:50 -0500

    electron setup

--- src/electron/main.js
+++ src/electron/main.js
@@ -1,7 +1,17 @@
 import { app, BrowserWindow } from "electron";
 import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.on("ready", () => {
-  const mainWindow = new BrowserWindow({});
-  mainWindow.loadFile( path.join(__dirname, "/dist-react/index.html") );
-})
+  const mainWindow = new BrowserWindow({
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  });
+
+  mainWindow.loadFile(path.resolve(__dirname, "..", "..", "dist-react", "index.html"));
+});
