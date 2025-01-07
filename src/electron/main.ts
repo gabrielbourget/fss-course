f9ef5b13c428a34b8245415514126294efe21cfd
Gabriel B <gabriel@Canadas-MacBook-Pro-2.local>
2025-01-07 09:56:11 -0500

    DX Improvements

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -1,6 +1,7 @@
 import { app, BrowserWindow } from "electron";
 import path from "path";
 import { fileURLToPath } from "url";
+import { isDev } from "./util.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -13,8 +14,13 @@ app.on("ready", () => {
     }
   });
 
-  console.log(`__dirname: ${__dirname}`);
-  console.log(`path created -> ${path.join(__dirname, "..", "dist-react", "index.html")}`);
+  if (isDev()) {
+    mainWindow.loadURL("http://localhost:5123");
+  } else {
+    mainWindow.loadFile(path.join(__dirname, "..", "dist-react", "index.html"));
+  }
 
-  mainWindow.loadFile(path.join(__dirname, "..", "dist-react", "index.html"));
+  // console.log(`__dirname: ${__dirname}`);
+  // console.log(`path created -> ${path.join(__dirname, "..", "dist-react", "index.html")}`);
+
 });
