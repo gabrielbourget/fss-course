7b9822510f2ca4fa6bf162b655498c5dc1d89f9b
Gabriel B <gabriel@Canadas-MacBook-Pro-2.local>
2025-01-08 13:05:19 -0500

    Define global types

---
+++ types.d.ts
@@ -0,0 +1,11 @@
+type TStatistics = {
+  totalStorage: number,
+  CPUUsage: number,
+  RAMUsage: number,
+}
+
+type TStaticData = {
+  totalStorage: number,
+  CPUModel: string,
+  totalMemoryGB: number,
+}
