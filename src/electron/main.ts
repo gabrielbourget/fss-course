268a7c833c95dca23ecc84ed553157ab8c200f21
Gabriel B <gaboxa@syntronic.com>
2025-01-08 21:56:41 -0500

    handling closing app

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -1,12 +1,8 @@
-import { app, BrowserWindow } from "electron";
-// import path from "path";
-// import { fileURLToPath } from "url";
+import { app, BrowserWindow, Tray } from "electron";
+import path from "path";
 import { ipcMainHandle, isDev } from "./util.js";
 import { getStaticData, pollResources } from "./resourceManager.js";
-import { getPreloadPath, getUIPath } from "./pathResolver.js";
-
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
+import { getAssetPath, getPreloadPath, getUIPath } from "./pathResolver.js";
 
 app.on("ready", () => {
   const mainWindow = new BrowserWindow({
@@ -24,4 +20,25 @@ app.on("ready", () => {
   pollResources(mainWindow);
 
   ipcMainHandle("getStaticData", () => getStaticData());
+
+  new Tray(path.join(getAssetPath(), process.platform === "darwin" ? "trayIconTemplate@8x.png" : "trayIconTemplate.png"));
+
+  handleCloseEvents(mainWindow);
 });
+
+const handleCloseEvents = (mainWindow: BrowserWindow) => {
+  let willClose = false;
+
+  if (willClose) return;
+
+  mainWindow.on("close", (e) => {
+    e.preventDefault();
+    mainWindow.hide();
+
+    if (app.dock) app.dock.hide();
+  });
+
+  app.on("before-quit", () => willClose = true);
+
+  mainWindow.on("show", () => willClose = false);
+};
