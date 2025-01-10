8965ae2bb6e276062caec88cd0415087a4d35986
Gabriel B <gaboxa@syntronic.com>
2025-01-10 14:41:15 -0500

    e2e test setups

---
+++ e2e/test.spec.ts
@@ -0,0 +1,60 @@
+import { test, expect, before, _electron } from "@playwright/test";
+
+let electronApp: Awaited<ReturnType<typeof _electron.launch>>;
+let mainPage: Awaited<ReturnType<typeof electronApp.firstWindow>>;
+
+const waitForPreloadScript = async () => {
+  return new Promise((resolve) => {
+    const interval = setInterval(async () => {
+      const electronBridge = await mainPage.evaluate(() => {
+        return (window as Window & { electron?: unknown }).electron;
+      });
+
+      if (electronBridge) {
+        clearInterval(interval);
+        resolve(true);
+      }
+    }, 100);
+  })
+}
+
+test.beforeEach(async () => {
+  electronApp = await _electron.launch({
+    args: ["."],
+    env: { NODE_ENV: "development" },
+  });
+
+  mainPage = await electronApp.firstWindow();
+  await waitForPreloadScript();
+});
+
+test.afterEach(async () => {
+  await electronApp.close();
+});
+
+test("page has title", async ({ page }) => {
+  await page.goto("https://playwright.dev");
+
+  await expect(page).toHaveTitle(/Playwright/);
+});
+
+test("custom frame should minimize the main window", async () => {
+  await mainPage.click("#minimize");
+  const isMinimized = await electronApp.evaluate((electron) => {
+    return electron.BrowserWindow.getAllWindows()[0].isMinimized();
+  });
+
+  expect(isMinimized).toBeTruthy();
+});
+
+test("should create a custom menu", async () => {
+  const menu = await electronApp.evaluate((electron) => {
+    return electron.Menu.getApplicationMenu();
+  });
+
+  expect(menu).not.toBeNull();
+  expect(menu?.items).toHaveLength(2);
+  expect(menu?.items[0].submenu?.items).toHaveLength(2);
+  expect(menu?.items[1].submenu?.items).toHaveLength(3);
+  expect(menu?.items[1].label).toBe("View");
+});
